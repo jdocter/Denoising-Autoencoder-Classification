1921dec1aa5859f3c84ebee384f6fa4984b6c21e
gongy <richardgong0@gmail.com>
2019-12-07 19:46:35 -0500

    Temp push

--- mnist.py
+++ mnist.py
@@ -1,17 +1,21 @@
 import keras
+import numpy as np
+from keras import initializers
 from keras.datasets import mnist
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
+import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 def show_train_image(image_index):
     print(y_train[image_index])
-    plt.imshow(x_train[image_index], cmap='Greys')
+    plt.show(x_train[image_index], cmap='Greys')
 
 def show_test_image(image_index):
     print(y_test[image_index])
-    plt.imshow(x_test[image_index], cmap='Greys')
+    plt.show(x_test[image_index], cmap='Greys')
 
 # Reshaping the array to 4-dims so that it can work with the Keras API
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
@@ -29,23 +33,25 @@ num_classes = 10
 batch_size = 128
 epochs = 2
 
+np.random.seed(42)
+my_init = initializers.glorot_uniform(seed=42)
+
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+visible = Input(shape=input_shape)
+x = Conv2D(10, kernel_size=3, activation='elu', padding='same', kernel_initializer=my_init)(visible)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Conv2D(20, kernel_size=3, activation='elu', padding='same', kernel_initializer=my_init)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Conv2D(30, kernel_size=3, activation='elu', padding='same', kernel_initializer=my_init)(x)
+x = GlobalMaxPooling2D()(x)
+output = Dense(y_train.shape[1], activation='softmax', name = 'value', kernel_initializer=my_init)(x)
 
-model.compile(loss=keras.losses.categorical_crossentropy,
+model = Model(inputs=visible, outputs=[output])
+
+model.compile(loss=[keras.losses.categorical_crossentropy],
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
