57bf2164f61b77d33a5bbdde38945766fdb02be2
gongy <richardgong0@gmail.com>
2019-12-07 19:31:28 -0500

    Initial MNIST train.

--- mnist.py
+++ mnist.py
@@ -1,8 +1,9 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-print(x_train)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 def show_train_image(image_index):
     print(y_train[image_index])
@@ -12,17 +13,47 @@ def show_test_image(image_index):
     print(y_test[image_index])
     plt.imshow(x_test[image_index], cmap='Greys')
 
-def reshape_data():
-     # Reshaping the array to 4-dims so that it can work with the Keras API
-     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-     x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-     input_shape = (28, 28, 1)
-     # Making sure that the values are float so that we can get decimal points after division
-     x_train = x_train.astype('float32')
-     x_test = x_test.astype('float32')
-     # Normalizing the RGB codes by dividing it to the max RGB value.
-     x_train /= 255
-     x_test /= 255
+# Reshaping the array to 4-dims so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+# Making sure that the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+# Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255
+x_test /= 255
 
+input_shape = x_train[0].shape
+num_classes = 10
+batch_size = 128
+epochs = 2
 
-show_test_image(8)
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
